214f06ee2ca505f066315df3ada9fcf21e2bad66
Ayush Singh <ayushsingh1525@gmail.com>
2022-06-24 16:11:38 +0530

    added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
-import 'package:srmacadmia/Views/Attendance/AttendanceContainer.dart';
-import 'package:srmacadmia/models/Details.dart';
 
-import 'Controllers/Controllers.dart';
+import 'Views/Attendance/AttedancePage.dart';
 import 'Views/Attendance/Binding.dart';
 import 'Views/Login/Binding.dart';
 import 'Views/Login/LoginPage.dart';
 
-import 'package:shimmer/shimmer.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: 'envFiles/secret.env');
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+
+  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   runApp(const MyApp());
 }
@@ -32,6 +38,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
       initialRoute: '/login',
       defaultTransition: Transition.zoom,
       getPages: [
@@ -41,85 +48,12 @@ class MyApp extends StatelessWidget {
             binding: LoginBinding()),
         GetPage(
           name: '/attendance',
-          page: () => const MyHomePage(
+          page: () => const AttendancePage(
             title: 'Academia',
           ),
           binding: AttendanceBinding(),
         ),
       ],
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  static const id = 'MyHomePage';
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = Get.find<AcadDataController>();
-    return Scaffold(
-      backgroundColor: Colors.blue.shade100,
-      body: Center(
-        child: FutureBuilder<Details>(
-          future: controller
-              .getData(), // this now lives it its own AlbumController class
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.attendance!.length,
-                  itemBuilder: (context, index) {
-                    return AttendanceContainer(
-                      studentInfo: (snapshot.data)!,
-                      index: index,
-                    );
-                  });
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            // By default, show a loading spinner.
-            return SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Expanded(
-                    child: Shimmer.fromColors(
-                      baseColor: Colors.grey.shade300,
-                      highlightColor: Colors.grey.shade100,
-                      child: ListView.builder(
-                        itemBuilder: (_, __) => Container(
-                          margin: const EdgeInsets.all(7),
-                          height: MediaQuery.of(context).size.height * 0.22,
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15.0, vertical: 25.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15.0),
-                            color: Colors.grey.shade300,
-                          ),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                width: 48.0,
-                                height: 68.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                        ),
-                        itemCount: 6,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
     );
   }
 }
